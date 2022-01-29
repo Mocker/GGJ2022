78d106cb7a1dc7e1a3beea7f26bc69b14d718dc0
Ryan <ryan@ryanguthrie.com>
2022-01-28 17:08:02 -0800

    load firebase config from ignored filed, merged ui and login branches

--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+import { firebaseConfig } from "../../config/firebaseConfig";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
